baf771fdddc594ba471770da49223684d520321e
Kimgaegeol <flying0305@naver.com>
2025-02-11 16:45:34 +0900

    v1.4

--- day2.py
+++ day2.py
@@ -1,4 +1,5 @@
-import math
+def my_pow(x,y) -> float:
+    return x**y
 
 def is_prime(num) -> bool:
     """
@@ -8,7 +9,7 @@ def is_prime(num) -> bool:
     """
     if num >= 2:
         i = 2
-        while(i <= int(math.pow(num,0.5)) + 1 ):
+        while(i <= int(my_pow(num,0.5)) + 1 ):
             if num % i == 0:
                 return False
             i += 1
@@ -32,3 +33,4 @@ while(i <= endNum):
 # v1.1) for -> while
 # v1.2) while 구문으로 구간 소수를 출력하는 프로그램 작성
 # v1.3) ** 대신 pow 함수
+# v1.4) my_pow함수 만들기
