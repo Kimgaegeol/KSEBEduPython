571f1deeac538c49a308a33966dfd957419fd9ba
Kimgaegeol <flying0305@naver.com>
2025-02-11 11:09:35 +0900

    v0.2

---
+++ day2.py
@@ -0,0 +1,23 @@
+# dan  =  int(input("Input dan : "))
+#
+# # for dan in range(2,10,1):
+# for i in range(1,10):
+#     print(f"{dan} * {i} = {dan*i}")
+
+# 소수 찾기 프로그램
+n = int(input("Input number : "))
+is_prime = True
+
+if  n > 1:
+    for i in range(2, n):
+        if n % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        print(f"{n} is prime number")
+    else:
+        print(f"{n} is NOT prime number")
+
+else:
+    print(f"{n} is NOT prime number")
