7622680123f6c14c979901006718d33e42ff1366
Kimgaegeol <flying0305@naver.com>
2025-02-13 14:27:55 +0900

    v4.2

--- day04.py
+++ day04.py
@@ -229,7 +229,18 @@ import time
 # print(tuple(map(abs,ns)))
 
 # ====================================================================================================================
+import time
 
+def time_decorator(func):
+    def wrapper(*arg):
+        s = time.time()
+        r = func(*arg)
+        e = time.time()
+        print(f'실행시간 : {e-s}초')
+        return r
+    return wrapper
+
+@time_decorator
 def factorial_repetition(n) -> int:
     result = 1
     for i in range(2, n+1):
@@ -239,10 +250,12 @@ def factorial_repetition(n) -> int:
 number = int(input())
 print(f'{number}! = {factorial_repetition(number)} ')
 
-s = time.time()
-print(f"{number}! = {factorial_repetition(number)}")
-e = time.time()
-print(e-s)
+# s = time.time()
+# print(f"{number}! = {factorial_repetition(number)}")
+# e = time.time()
+# print(e-s)
+
+
 
 # ====================================================================================================================
 
